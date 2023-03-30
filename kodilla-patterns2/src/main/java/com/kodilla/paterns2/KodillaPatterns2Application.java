a46fe7b4cbeb473c94d2e64b0be137370dfe8c19
lukstasko <119363165+lukstasko@users.noreply.github.com>
2023-03-30 22:51:34 +0200

    20.2 Added pizza decorators.

--- kodilla-patterns2/src/main/java/com/kodilla/paterns2/KodillaPatterns2Application.java
+++ kodilla-patterns2/src/main/java/com/kodilla/paterns2/KodillaPatterns2Application.java
@@ -1,4 +1,4 @@
-package com.kodilla.paterns2.facade;
+package com.kodilla.paterns2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
